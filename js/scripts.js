009b4486101afc4b35947b02137497eb4befc75f
AmanKhys <amankhys2112@gmail.com>
2024-10-18 11:11:20 +0530

    make form validation using regex and addEventListener; give styles to  errorMesseges;

--- js/scripts.js
+++ js/scripts.js
@@ -44,30 +44,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
-// form validation
-
-function validateForm() {
-    isValid = true;
-    const email = document.getElementById("email").value.trim();
-    const number = document.getElementById("number").value.trim();
-    const name = document.getElementById("name").value.trim();
-
-    let validEmail = /^[a-zA-Z]{1,}[a-zA-Z0-9]*@[a-zA-Z]{1,}\.[a-zA-Z]{1,}$/;
-    let validName = /^[a-zA-Z]{1,0}$/;
-
-    if (!validEmail.test(email)) {
-        document.getElementById('emailError').textContent = 'enter a valid mail';
-        isValid = false;
-    }
-    if (number < 1000000000 && number >= 10000000000) {
-        document.getElementById('numberError').textContent = 'enter a 10 digit number';
-        isValid = false;
-    }
-    if (!validName.test(name)) {
-        document.getElementById("nameError").textContent = 'enter a valid name';
-        isValid = false;
-    }
-
-    return isValid;
-}
